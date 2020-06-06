6e1fe1f5f138f82d8d83964e88ba96955f3c07f7
tarek10 <tarek.bettaieb@esprit.tn>
2020-06-06 15:39:09 +0100

    EmployeeService

---
+++ ConsomiTounsi.service/EmployeeService.cs
@@ -0,0 +1,23 @@
+﻿using ConsomiTounsi.data.Infrastructure;
+using ConsomiTounsi.domain.Entities;
+using Service.Pattern;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsomiTounsi.service
+{
+    public class EmployeeService : Service<Employee>, IEmployeeService
+    {
+
+            static IDataBaseFactory Factory = new DataBaseFactory();
+            static IUnitOfWork utk = new UnitOfWork(Factory);
+            public EmployeeService() : base(utk)
+            {
+
+            }
+
+    }
+}
